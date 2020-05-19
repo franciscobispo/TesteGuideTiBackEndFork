bf0aed5cb78fde0bac95eb6881aa50474e53df2a
user <user@DESKTOP-CRUZ>
2020-05-19 14:28:57 -0300

    Initial

--- Service/Services/AutorService.cs
+++ Service/Services/AutorService.cs
@@ -28,38 +28,18 @@ namespace Service.Services
                     var nomeSplitted = itemListaAutores.Nome.Split(" ");
 
                     if (nomeSplitted.Count() == 1)
-                    { //1ª regra: se houver apenas uma parte no nome, ela deve ser apresentada em letras maiúsculas (sem vírgula)
-                        itemListaAutores.Nome = itemListaAutores.Nome.ToUpper();
-                    }
-                    else if (nomeSplitted.Count() >= 2 &&
-                                //(nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("FILHO") ||
-                                // nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("FILHA") ||
-                                // nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("NETO") ||
-                                // nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("NETA") ||
-                                // nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("SOBRINHO") ||
-                                // nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("SOBRINHA") ||
-                                // nomeSplitted[nomeSplitted.Count() - 1].ToUpper().Contains("JUNIOR"))
-                                parentescosArray.Any(x => x == nomeSplitted[nomeSplitted.Count() - 1])
-                                 )
+                        itemListaAutores.Nome = itemListaAutores.Nome.ToUpper();                    
+                    else if (nomeSplitted.Count() >= 2 && parentescosArray.Any(x => x == nomeSplitted[nomeSplitted.Count() - 1]))
                     {
-                        if (nomeSplitted.Count() == 2)//se a entrada for "Joao Neto" , a saída deve ser "NETO, Joao"
-                        {
+                        if (nomeSplitted.Count() == 2)
                             itemListaAutores.Nome = (nomeSplitted[1].ToUpper() + ", " + PrimeiraLetraMaiuscula(nomeSplitted[0].ToLower()));
-                        }
                         else
                         {                            
                             List<string> restoNome = new List<string>();
                             for (int index = 0; index < nomeSplitted.Count() - 2; index++)
                             {
-                                if (
-                                    //nomeSplitted[index].ToUpper().Contains("DA") || nomeSplitted[index].ToUpper().Contains("DE") ||
-                                    //nomeSplitted[index].ToUpper().Contains("DO") || nomeSplitted[index].ToUpper().Contains("DAS") ||
-                                    //nomeSplitted[index].ToUpper().Contains("DOS")
-                                    preposicoesArray.Any(x => x == nomeSplitted[index].ToUpper())
-                                    )
-                                {
+                                if (preposicoesArray.Any(x => x == nomeSplitted[index].ToUpper()))
                                     restoNome.Add(nomeSplitted[index].ToLower());
-                                }
                                 else
                                     restoNome.Add(PrimeiraLetraMaiuscula(nomeSplitted[index].ToLower()));
                             }                            
@@ -67,26 +47,18 @@ namespace Service.Services
                         }
                     }
                     else
-                    { //regra: nome >= 2 index e não possui sufixos DA, DOS....
-                        
+                    {                        
                         List<string> restoNome = new List<string>();
                         for (int index = 0; index < nomeSplitted.Count() - 1; index++)
                         {
-                            if (
-                                 //nomeSplitted[index].ToUpper().Contains("DA") || nomeSplitted[index].ToUpper().Contains("DE") ||
-                                 //nomeSplitted[index].ToUpper().Contains("DO") || nomeSplitted[index].ToUpper().Contains("DAS") ||
-                                 //nomeSplitted[index].ToUpper().Contains("DOS")
-                                 preposicoesArray.Any(x => x == nomeSplitted[index].ToUpper())
-                                 )
-                            {
+                            if (preposicoesArray.Any(x => x == nomeSplitted[index].ToUpper()))
                                 restoNome.Add(nomeSplitted[index].ToLower());
-                            }
                             else
                                 restoNome.Add(PrimeiraLetraMaiuscula(nomeSplitted[index].ToLower()));
                         }
+
                         itemListaAutores.Nome = nomeSplitted[nomeSplitted.Count() - 1].ToUpper() + ", " + string.Join(" ", restoNome);
                     }
-
                 }
             }
 
